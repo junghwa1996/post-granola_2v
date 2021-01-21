005eed636b868b6310d644d85d1fb2f7e5d4f2c7
junghwa1996 <59001439+junghwa1996@users.noreply.github.com>
2021-01-21 14:34:37 +0900

    업로드

---
+++ src/js/common.js
@@ -0,0 +1,267 @@
+// youtube API 불러옴
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 플레이어변수 설정
+var youTubePlayer1;
+
+function onYouTubeIframeAPIReady() {
+	youTubePlayer1 = new YT.Player('youTubePlayer1', {
+		width: '1280',
+		height: '720',
+		videoId: 'ApZhAA7qWF0',
+		playerVars: {rel: 0},//추천영상 안보여주게 설정
+		events: {
+			'onReady': onPlayerReady, //로딩할때 이벤트 실행
+			'onStateChange': onPlayerStateChange //플레이어 상태 변화시 이벤트실행
+		}
+	});//youTubePlayer1셋팅
+}
+
+function onPlayerReady(event) {
+	// event.target.playVideo();//자동재생
+	//로딩할때 실행될 동작을 작성한다.
+}
+
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.PLAYING) {
+	//플레이어가 재생중일때 작성한 동작이 실행된다.
+	}
+}
+
+function fixedNav() {
+    if ($(document).scrollTop() > $('header h1').height()) {
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+	}
+	if ($(document).scrollTop() > $('#event1').offset().top - $('nav').height() - 1 && $(document).scrollTop() < $('#event2').offset().top - $('nav').height() - 1) {
+		$('.nav-event1').addClass('active');
+		$('.nav-event2').removeClass('active');
+		$('.nav-event3').removeClass('active');
+	} else if ($(document).scrollTop() > $('#event2').offset().top - $('nav').height() - 1 && $(document).scrollTop() < $('#event3').offset().top - $('nav').height() - 1) {
+		$('.nav-event1').removeClass('active');
+		$('.nav-event2').addClass('active');
+		$('.nav-event3').removeClass('active');
+	} else if ($(document).scrollTop() > $('#event3').offset().top - $('nav').height() - 1 && $(document).scrollTop() < $('.notice').offset().top - $('nav').height()) {
+		$('.nav-event1').removeClass('active');
+		$('.nav-event2').removeClass('active');
+		$('.nav-event3').addClass('active');
+	} else {
+		$('.nav-event1').removeClass('active');
+		$('.nav-event2').removeClass('active');
+		$('.nav-event3').removeClass('active');
+	}
+}
+
+const targetElement = document.querySelector('#modal');
+
+function modalOpen(modal) {
+    $('body').addClass('modal-active');
+    $('#modal').addClass('active');
+    $(modal).removeClass('hidden');
+	$(modal + '-page1').removeClass('hidden');
+	bodyScrollLock.disableBodyScroll(targetElement);
+}
+
+function modalNext(modal, current, next) {
+	$(modal + '-' + current).addClass('hidden');
+	if (next === 'end') {
+		modalClose();
+	} else {
+		$(modal + '-' + next).removeClass('hidden');
+	}
+}
+
+function modalCheck(modal, current, next) {
+    var modalForms = [];
+    modalForms.push($(modal + '-' + current + ' .form-name input'));
+    modalForms.push($(modal + '-' + current + ' .form-tel input'));
+    modalForms.push($(modal + '-' + current + ' .form-address input:first-of-type'));
+	modalForms.push($(modal + '-' + current + ' .form-agree input'));
+    var regexPhone = /^(010)[0-9]{4}[0-9]{4}$/;
+    if (modalForms[0].val() === '') {
+        alert('정보를 입력해주세요!');
+        modalForms[0].focus();
+        modalForms[0].scroll();
+        return false;
+    } else if (modalForms[1].val() === '') {
+        alert('정보를 입력해주세요!');
+        modalForms[1].focus();
+        modalForms[1].scroll();
+        return false;
+    } else if (modalForms[2].val() === '') {
+        alert('정보를 입력해주세요!');
+        $(modal + '-' + current + ' .form-address-find').focus();
+        $(modal + '-' + current + ' .form-address-find').scroll();
+        return false;
+    } else if (!regexPhone.test(modalForms[1].val())) {
+        alert('연락처를 확인해주세요.');
+        modalForms[1].focus();
+        modalForms[1].scroll();
+        return false;
+    } else if (modalForms[3].prop('checked') === false) {
+        alert('개인 정보 취급/이용 약관에 동의해주세요.');
+        modalForms[3].focus();
+        modalForms[3].scroll();
+        return false;
+    } else {
+        alert('참여가 완료 되었습니다.');
+        modalNext(modal, current, next);
+    }
+}
+
+function modalClose() {
+    $('body').removeClass('modal-active');
+    $('#modal').removeClass('active');
+    $('.modal-container').addClass('hidden');
+	$('.modal-page').addClass('hidden');
+	$('#modal input[type="text"]').val('');
+	$('#modal input[type="tel"]').val('');
+	$('#modal textarea').val('');
+	$('#modal input[type="checkbox"]').prop('checked', false);
+	bodyScrollLock.enableBodyScroll(targetElement);
+}
+
+function hashtagCopy() {
+    var dummy = document.createElement("textarea");
+    document.querySelector('.hash-dummy').appendChild(dummy);
+    dummy.value = '#포스트콘푸라이트 #콘푸라이트 #우리가족식사 #아빠표플레이하우스 #콘푸라이트플레이하우스';
+    dummy.select();
+    document.execCommand("copy");
+	document.querySelector('.hash-dummy').removeChild(dummy);
+	alert('해시태그 복사가 완료되었습니다.');
+}
+
+$('header nav').on('click', 'a[href^="#"]', function(event) {
+	event.preventDefault();
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top - $('nav').height() + 1
+	}, 500);
+});
+
+$(document).ready(function() {
+    $(".movie-thumb").on("click", function() {
+        $(this).hide();
+        youTubePlayer1.playVideo();//재생
+    });
+});
+
+$(window).scroll(function() {
+	fixedNav();
+});
+
+$('.button-event1').click(function() {
+	modalOpen('.modal-event1');
+});
+
+$('.modal-event1-page1 .modal-submit').click(function() {
+	modalNext('.modal-event1', 'page1', 'page2');
+});
+
+$('.modal-event1-page2 .modal-submit').click(function() {
+	modalCheck('.modal-event1', 'page2', 'end');
+});
+
+$('input[name="eventSelect"]').change(function() {
+	$('.event2-select li').removeClass('active');
+	if ($(this).is(':checked') === true) {
+		$(this).parent('li').addClass('active');
+	}
+})
+
+$('.button-event2').click(function() {
+	if ($('input[name="eventSelect"]').is(':checked') === false) {
+		alert('보기를 선택해주세요!');
+	} else {
+		modalOpen('.modal-event2');
+		if ($('#eventSelect6').is(':checked') === false) {
+			modalNext('.modal-event2', 'page1', 'page2');
+		}
+	}
+});
+
+$('.modal-event2-page1 .modal-submit').click(function() {
+	modalNext('.modal-event2', 'page1', 'page2');
+});
+
+$('.modal-event2-page2 .modal-submit').click(function() {
+	modalCheck('.modal-event2', 'page2', 'loading');
+	if (!document.querySelector('.modal-event2-loading').classList.contains('hidden')) {
+		window.setTimeout(function() { modalNext('.modal-event2', 'loading', 'page3-1') }, 3000);
+	}
+});
+
+$('.modal-event2-page3 .modal-submit').click(function() {
+	modalNext('.modal-event2', 'page3', 'page4');
+});
+
+$('.modal-event2-page4 .modal-submit').click(function() {
+	modalNext('.modal-event2', 'page4', 'end');
+});
+
+$('.button-event3-1').click(function() {
+	hashtagCopy();
+});
+
+$('.button-event3-2').click(function() {
+	modalOpen('.modal-event3');
+});
+
+$('.modal-event3-page1 .modal-submit').click(function() {
+	modalNext('.modal-event3', 'page1', 'page3');
+});
+/*
+$('.modal-event3-page2 .modal-submit').click(function() {
+	modalNext('.modal-event3', 'page2', 'page3');
+});
+*/
+$('.modal-event3-page3 .modal-back').click(function() {
+	modalNext('.modal-event3', 'page3', 'page1');
+});
+
+$('.modal-event3-page3 .modal-submit').click(function() {
+	modalNext('.modal-event3', 'page3', 'page4');
+});
+
+$('.modal-event3-page4 .modal-submit').click(function() {
+	modalCheck('.modal-event3', 'page4', 'end');
+});
+
+$('.button-gallery').click(function() {
+	modalOpen('.modal-gallery');
+})
+
+$('.modal-gallery-page1 .modal-submit').click(function() {
+	modalNext('.modal-gallery', 'page1', 'end');
+});
+
+$('.modal-close').click(function() {
+	modalClose();
+});
+
+$('.form-read-button').click(function() {
+	if (!$(this).hasClass('active')) {
+		$('.form-read').addClass('active');
+		$('.form-read-button').addClass('active');
+	} else {
+		$('.form-read').removeClass('active');
+		$('.form-read-button').removeClass('active');
+	}
+});
+
+var mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+	autoHeight: true,
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+})
+
+$('.button-result1').click(function() {
+	modalOpen('.modal-result1');
+	mySwiper.update();
+});
